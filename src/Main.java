61cf0c44342b137f0d7209f19f11b3f7b6f07dc0
Vladislav Kosenko <katko5@mail.ru>
2023-05-01 17:14:29 +0500

    Сhanged commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        int account = 200;
+        int replenishment = 1200;
+        int inTotal = account + replenishment;
+        System.out.println("Total on the account:" + inTotal);
 
-        System.out.println();
+        int bonus = 1;
+        int bonuses = replenishment / 100 * bonus;
+
+        if (replenishment > 100) {
+
+            bonuses = replenishment / 100 * bonus;
+        }
+
+        System.out.println("Number of bonus rubles:" + bonuses);
     }
 }
